c27089c0350843d1e983abf3bf39a37e5e4df373
Lucas Goes <lucasdegoes@outlook.com.br>
2019-09-08 22:15:13 -0300

    adicionando remoção de item no estoque

--- course-poo-ex3/src/application/Program.java
+++ course-poo-ex3/src/application/Program.java
@@ -27,9 +27,20 @@ public class Program {
 		System.out.println("Product Data: " +product);
 		
 		System.out.println();
-		System.out.println("Enter the number of products to be added in stock: ");
+		System.out.print("Enter the number of products to be added in stock: ");
 		int quantity = sc.nextInt();
 		product.addProduct(quantity);
+		
+		System.out.println();
+		System.out.println("Updated Data: " +product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProduct(quantity);
+		
+		System.out.println();
+		System.out.println("Updated Data: " +product);
 		
 		sc.close();
 	}
