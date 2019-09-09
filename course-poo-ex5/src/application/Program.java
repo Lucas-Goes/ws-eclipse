921f008528983ea1425551f47d2776f0c6fbe76a
Lucas Goes <lucasdegoes@outlook.com.br>
2019-09-09 12:36:29 -0300

    ex com entidade funcionario, com incremento de % por metódo

---
+++ course-poo-ex5/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee empl = new Employee();
+		
+		System.out.println("Enter the Name, Salary and Tax: ");
+		System.out.print("Name: ");
+		empl.name  = sc.nextLine();
+		
+		System.out.print("Salary: ");
+		empl.grossSalary  = sc.nextDouble();
+		
+		System.out.print("Tax: ");
+		empl.tax  = sc.nextDouble();
+		
+		System.out.println("Employee: " + empl.name + ", $" + empl.netSalary());
+		
+		System.out.print("Wich percentage to increase salary? ");
+		double p = sc.nextDouble();
+		double percentage = empl.increaseSalary(p);
+		
+		System.out.println("Updated data: " + empl.name + ", $" + percentage);
+		
+		
+		
+		sc.close();
+	}
+
+}
